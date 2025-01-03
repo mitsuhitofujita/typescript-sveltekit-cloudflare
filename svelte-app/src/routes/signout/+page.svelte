8f601e5402463ed626adf07dd2b8d068a02f5a6a
Mitsuhito Fujita <mail@mitsuhito.dev>
2025-01-03 08:14:08 +0000

    feature: add signout

---
+++ svelte-app/src/routes/signout/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { supabase } from '../../lib/supabase/client';
+
+	const signOut = async (): Promise<void> => {
+		const { error } = await supabase.auth.signOut();
+		if (error) {
+			console.error(error.message);
+		} else {
+			alert('Signout successful!');
+		}
+	};
+</script>
+
+<form on:submit|preventDefault={signOut}>
+	<button type="submit">Sign Out</button>
+</form>
