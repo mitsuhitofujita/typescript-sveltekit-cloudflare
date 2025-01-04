d41ea38e0f9a49ce22a646bdc92c2a4e71d57cc9
Mitsuhito Fujita <mail@mitsuhito.dev>
2025-01-04 15:18:02 +0000

    feat: add supabase server side auth

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # typescript-sveltekit-cloudflare
+
+
+npx supabase login
+
--- svelte-app/src/app.d.ts
+++ svelte-app/src/app.d.ts
@@ -1,13 +1,24 @@
+import type { Session, SupabaseClient, User } from '@supabase/supabase-js';
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
 	namespace App {
-        interface Platform {
-            env: Env
-            cf: CfProperties
-            ctx: ExecutionContext
-        }
-    }
+		interface Platform {
+			env: Env;
+			cf: CfProperties;
+			ctx: ExecutionContext;
+		}
+		interface Locals {
+			supabase: SupabaseClient;
+			safeGetSession: () => Promise<{ session: Session | null; user: User | null }>;
+			session: Session | null;
+			user: User | null;
+		}
+		interface PageData {
+			session: Session | null;
+		}
+	}
 }
 
 export {};
